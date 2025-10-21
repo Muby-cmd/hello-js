a6a0bd5c12288dbbb5aae84800ec1ccd9193f897
El-Mubaraq <elmubaraqajibola@gmail.com>
2025-10-21 12:45:31 +0100

    new

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // console.log("hello, world!");
 
-const math = require("./math.js");
+// const math = require("./math.js");
 
-console.log(math.add(3, 7));
+// console.log(math.add(3, 7));
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello from Raw Node.js!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found :{");
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Raw server running on http://localhost:${PORT}`);
+});
